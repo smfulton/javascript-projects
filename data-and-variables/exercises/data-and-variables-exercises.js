9aaff43f6d81e608bafe25dc820007a70cfad522
smfulton <sfulton1996@gmail.com>
2024-06-12 17:53:30 -0500

    Updated for b) Class 1 Exercises: Data and Variables.

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -1,11 +1,25 @@
 // Declare and assign the variables below
-
+let shuttleName = "Determination",
+shuttleSpeedMPH = 17500,
+kilometersToMars = 225000000,
+kilometersToMoon = 384400,
+milesPerKilometer = 0.621;
 // Use console.log to print the 'typeof' each variable. Print one item per line.
-
+console.log(typeof shuttleName)
+console.log(typeof shuttleSpeedMPH)
+console.log(typeof kilometersToMars)
+console.log(typeof kilometersToMoon)
+console.log(typeof milesPerKilometer);
 // Calculate a space mission below
-
+let milesToMars = kilometersToMars * milesPerKilometer;
+let hoursToMars = milesToMars / shuttleSpeedMPH;
+let daysToMars = hoursToMars / 24;
 // Print the results of the space mission calculations below
-
+console.log(shuttleName, "will take", daysToMars, "days to reach Mars.");
 // Calculate a trip to the moon below
-
+let milesToMoon = kilometersToMoon * milesPerKilometer;
+let hoursToMoon = milesToMoon / shuttleSpeedMPH;
+let daysToMoon = hoursToMoon / 24;
 // Print the results of the trip to the moon below
+console.log(shuttleName, "will take",daysToMoon,"days to reach the Moon.");
+console.log(1 + 5 % 3);
--- how-to-write-code/consolelogexamples01.js
+++ how-to-write-code/consolelogexamples01.js
@@ -4,3 +4,4 @@ console.log("What","do","commas","do?");
 console.log("Does", "adding",      "space", "matter?");
 console.log('Launch' + 'Code');
 console.log("LaunchCode was founded in", 2013);
+console.log(2**2**3*3);
